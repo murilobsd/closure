f63e4846cc6aa9fb52846c346bbe238c079448e6
msi <mbsd@m0x.ru>
2021-06-19 22:40:18 -0300

    fix test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
@@ -6,7 +7,7 @@ where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    value: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -16,16 +17,16 @@ where
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            value: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        match self.value.get(&arg) {
+            Some(v) => *v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.value.insert(arg, v);
                 v
             }
         }
@@ -42,22 +43,14 @@ fn generate_workout(intensity: u32, random_number: u32) {
     if intensity < 25 {
         println!("Today, do {} pushups!", expensive_result.value(intensity));
         println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else if random_number == 3 {
+        println!("Take a break today! Remember to stay hydrated");
     } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result.value(intensity)
-            );
-        }
+        println!(
+            "Today, run for {} minutes!",
+            expensive_result.value(intensity)
+        );
     }
-}
-
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculation slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
 }
 
 fn main() {
@@ -65,4 +58,18 @@ fn main() {
     let simulated_random_value = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_value);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+        let _v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
+    }
 }
