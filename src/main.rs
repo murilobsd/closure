ad572a8d55bb6b5f3e49b26a3234f3ccc7edcd8a
msi <mbsd@m0x.ru>
2021-06-19 21:40:33 -0300

    example pattern memoization

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
 use std::thread;
 use std::time::Duration;
 
-fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_value = 7;
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
 
-    generate_workout(simulated_user_specified_value, simulated_random_value);
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    // let expensive_result = simulated_expensive_calculation(intensity);
-    let expensive_closure = |num: u32| -> u32 {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculation slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
@@ -32,4 +58,11 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculation slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_value = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_value);
 }
